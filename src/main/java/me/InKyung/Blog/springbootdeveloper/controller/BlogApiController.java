fa8813311f4a970878cb7850c29234ff1654d088
SummerToday <kom347@naver.com>
2024-02-11 16:27:38 +0900

    6.6 블로그 글 삭제 API 구현

--- src/main/java/me/InKyung/Blog/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/InKyung/Blog/springbootdeveloper/controller/BlogApiController.java
@@ -45,4 +45,15 @@ public class BlogApiController {
                 .body(new ArticleResponse(article));
     }
 
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(
+            @PathVariable long id){
+        blogService.delete(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
 }
