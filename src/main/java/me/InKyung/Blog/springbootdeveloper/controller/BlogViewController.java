1a8a6b4c751660ff54bf5e45f0e249e6a30b78b2
SummerToday <kom347@naver.com>
2024-02-15 16:53:41 +0900

    7.3 블로그 글 상세 뷰 구현

--- src/main/java/me/InKyung/Blog/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/InKyung/Blog/springbootdeveloper/controller/BlogViewController.java
@@ -1,5 +1,7 @@
 package me.InKyung.Blog.springbootdeveloper.controller;
 
+import me.InKyung.Blog.springbootdeveloper.domain.Article;
+import me.InKyung.Blog.springbootdeveloper.dto.ArticleViewResponse;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 
@@ -7,6 +9,7 @@ import me.InKyung.Blog.springbootdeveloper.dto.ArticleListViewResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import me.InKyung.Blog.springbootdeveloper.service.*;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,5 +28,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
