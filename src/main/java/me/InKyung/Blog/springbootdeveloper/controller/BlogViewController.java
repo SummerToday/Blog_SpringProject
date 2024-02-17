082a6992a83fd0332b4c6066e4bf10886808f33c
SummerToday <kom347@naver.com>
2024-02-17 14:29:05 +0900

    7.5 블로그 글 수정 기능 구현

--- src/main/java/me/InKyung/Blog/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/InKyung/Blog/springbootdeveloper/controller/BlogViewController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import me.InKyung.Blog.springbootdeveloper.service.*;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -36,5 +37,16 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
