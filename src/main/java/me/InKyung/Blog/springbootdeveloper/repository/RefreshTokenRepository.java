94d23a977f527fc30c44a1de56b359a3a83752fa
SummerToday <kom347@naver.com>
2024-03-01 22:48:43 +0900

    ~ 9.2 JWT 서비스 구현

---
+++ src/main/java/me/InKyung/Blog/springbootdeveloper/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package me.InKyung.Blog.springbootdeveloper.repository;
+
+import me.InKyung.Blog.springbootdeveloper.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
