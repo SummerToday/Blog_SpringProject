35cc277df56235a4a1f6f50e63c557325d2bbb7f
SummerToday <kom347@naver.com>
2024-06-27 17:48:43 +0900

    Reapply "13. 데이터 유효성 검사 구현 (벡엔드)"
    This reverts commit aec40ffdf0ceec8d726eae37ac70f32d6cd072b8.

--- src/main/java/me/InKyung/Blog/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/InKyung/Blog/springbootdeveloper/dto/AddArticleRequest.java
@@ -5,13 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.InKyung.Blog.springbootdeveloper.domain.Article;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @NoArgsConstructor //기본 생성자 추가
 @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자 추가
 @Getter
 public class AddArticleRequest { // 서비스계층에서 요청 받은 데이터를 엔티티로 생성.
 
+    @NotNull
+    @Size(min = 1, max = 10)
     private String title;
+
+    @NotNull
     private String content;
 
     public Article toEntity(String author) { // 생성자를 사용해 객체 생성
